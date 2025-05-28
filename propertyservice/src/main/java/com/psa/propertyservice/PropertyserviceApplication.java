50fcf67ad30a9960f81e21fe6e979691c329cce5
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-05-29 04:13:19 +0530

    eureka server added to main

--- propertyservice/src/main/java/com/psa/propertyservice/PropertyserviceApplication.java
+++ propertyservice/src/main/java/com/psa/propertyservice/PropertyserviceApplication.java
@@ -2,8 +2,10 @@ package com.psa.propertyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PropertyserviceApplication {
 
 	public static void main(String[] args) {
