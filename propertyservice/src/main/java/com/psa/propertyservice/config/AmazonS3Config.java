a8fb883acbbc78f3d4a7b0609eaa6dd4efc2ffda
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-05-30 02:08:20 +0530

    fixed bugs

--- propertyservice/src/main/java/com/psa/propertyservice/config/AmazonS3Config.java
+++ propertyservice/src/main/java/com/psa/propertyservice/config/AmazonS3Config.java
@@ -6,7 +6,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AmazonS3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
