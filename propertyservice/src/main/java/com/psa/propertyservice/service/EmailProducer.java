d2b994614cf0e86e72ebb112c3f72d6728ec6994
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-05-31 07:07:23 +0530

    created email producer

---
+++ propertyservice/src/main/java/com/psa/propertyservice/service/EmailProducer.java
@@ -0,0 +1,19 @@
+package com.psa.propertyservice.service;
+
+import com.psa.propertyservice.dto.EmailRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailProducer {
+
+    @Autowired
+    private KafkaTemplate<String, EmailRequest> kafkaTemplate;
+
+    private static final String TOPIC = "send_email";
+
+    public void sendEmail(EmailRequest request) {
+        kafkaTemplate.send(TOPIC, request);
+    }
+}
