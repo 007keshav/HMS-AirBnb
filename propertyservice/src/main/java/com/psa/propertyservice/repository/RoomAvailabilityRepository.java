b5e6370b42624a0465c11ea7ffc8d9989c50eb2b
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-06-17 03:25:37 +0530

    added query to find room availability

--- propertyservice/src/main/java/com/psa/propertyservice/repository/RoomAvailabilityRepository.java
+++ propertyservice/src/main/java/com/psa/propertyservice/repository/RoomAvailabilityRepository.java
@@ -2,10 +2,17 @@ package com.psa.propertyservice.repository;
 
 import com.psa.propertyservice.entity.RoomAvailability;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface RoomAvailabilityRepository extends JpaRepository<RoomAvailability,Long> {
     public List<RoomAvailability> findByRoomId(long id);
+
+    @Query("SELECT ra FROM RoomAvailability ra WHERE ra.room.id = :id AND ra.availableDate = :date")
+    RoomAvailability findByRoomIdAndAvailableDate(@Param("id") Long id, @Param("date") LocalDate date);
+
 
 }
