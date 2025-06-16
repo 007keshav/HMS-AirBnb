db9bfe253559fbca85e06a4b260a35b5eee7299d
Keshav Krishn <keshavkrishn1006@gmail.com>
2025-06-17 03:19:18 +0530

    in jwt authentication added new openapiendpoints

--- API-Gate/src/main/java/com/psa/API_Gate/filter/JwtAuthenticationFilter.java
+++ API-Gate/src/main/java/com/psa/API_Gate/filter/JwtAuthenticationFilter.java
@@ -22,7 +22,8 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
     private static final List<String> openApiEndpoints = List.of(
             "/auth/api/v1/auth/login",
-            "/auth/api/v1/auth/register"
+            "/auth/api/v1/auth/register",
+            "property/api/v1/property/search-property"
 
     );
 
